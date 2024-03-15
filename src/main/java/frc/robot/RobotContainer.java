0c52904cebc89f4cf11845e717895595a668aba8
Pickles888 <verysaltypickles@gmail.com>
2024-03-15 16:35:59 -0700

    Added Test Motor Functionality (run with a)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,13 +6,25 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.commands.SetTestMotorSpeed;
+import frc.robot.subsystems.TestMotorSubsystem;
 
 public class RobotContainer {
+
+  private CommandXboxController m_controller = new  CommandXboxController(0);
+  private TestMotorSubsystem m_testMotorSubsystem = new TestMotorSubsystem();
+  
+
   public RobotContainer() {
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    m_controller.a().onTrue(
+      new SetTestMotorSpeed(m_testMotorSubsystem, 0.8)
+    );
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
